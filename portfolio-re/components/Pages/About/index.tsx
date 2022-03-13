1b94e369eb616e488e55a79035fe18dc6474f7ce
Sean Christian Lozana <lozanasc@gmail.com>
2022-03-13 14:55:45 +0800

    Completed about page

--- portfolio-re/components/Pages/About/index.tsx
+++ portfolio-re/components/Pages/About/index.tsx
@@ -1,4 +1,5 @@
-import { Stack, Heading, Text, Divider } from "@chakra-ui/react";
+import { Stack, Heading, Text, Divider, useBreakpointValue } from "@chakra-ui/react";
+import { Chakraui, Nextdotjs } from '@icons-pack/react-simple-icons';
 import { ProfileDesktop, ProfileMobile } from './components';
 
 type AboutProps = {
@@ -6,44 +7,87 @@ type AboutProps = {
 }
 
 const About = ({colorMode}:AboutProps) => {
+
+  const iconSize = useBreakpointValue([24, 32, 32])
+
   return (
     <Stack
       w="100%"
       m="auto"
-      direction={['column-reverse', 'column-reverse', 'row']}
-      justify="space-evenly"
-    >
+      direction={['column-reverse', 'column', 'row']}
+      justify="space-evenly">
       <Stack
+        m="auto"
         direction="column"
         w={['100%', '100%', '50%']}
-        m="auto"
-        spacing={4}
-      >
+        spacing={4}>
         <Heading
-          fontSize={['lg', 'xl', '4xl']}
+          fontSize={['lg', 'xl', 'lg', '3xl']}
+          color={colorMode === 'light' ?  'gray.700' : 'white'}
+          alignSelf={["center", null, "flex-start"]}
         >
           <span style={{fontWeight: 'normal'}}>I&apos;m </span>
           Sean Christian Lozana<span style={{color: "#F093AF"}}>.</span>
         </Heading>
         <Divider
-           width={['80%', '85%', '75%']}
+          dropShadow={"md"}
+          borderColor={colorMode === 'light' ?  'gray.700' : 'white'}
+          width={['80%', '85%', '75%']}
+          alignSelf={["center", null, "flex-start"]}
         />
         <Text 
           as="h4" 
-          fontSize={['sm', 'xl', '2xl']} fontWeight="light"
+          fontSize={['lg', 'xl', 'lg', '2xl']}
+          fontWeight="light"
           w={['90%', '85%', '80%']}
           color={colorMode === 'light' ?  'gray.700' : 'white'}
+          alignSelf={["center", null, "flex-start"]} textAlign={["center", null, "left"]}
         >
           I&apos;m a senior <span style={{color: "#F093AF", fontWeight: "bold"}}>Computer Science</span> student,
-          currently exploring the field of Web & Mobile design to development 
+          currently exploring the field of Web & Mobile development 
           and is looking for internship opportunities to <span style={{fontWeight: "bold"}}>learn and grow</span>
           <span style={{color: "#F093AF"}}>.</span>
         </Text>
         <Divider
-           width={['80%', '85%', '75%']}
+          dropShadow={"md"}
+          borderColor={colorMode === 'light' ?  'gray.700' : 'white'}
+          width={['80%', '85%', '75%']}
+          alignSelf={["center", null, "flex-start"]}
         />
+        <Stack direction="row" alignSelf={["center", null, "flex-start"]}>
+          <Text 
+            as="h4"
+            fontSize={['lg', 'xl', 'lg', '2xl']}
+            fontWeight="light"
+            color={colorMode === 'light' ?  'gray.700' : 'white'}
+          >
+            made with 
+          </Text>
+          <Chakraui color={colorMode === 'light' ? '#718096' : '#E2E8F0'} height={iconSize} width={iconSize}/>
+          <Text 
+            as="h4"
+            fontSize={['lg', 'xl', 'lg', '2xl']}
+            fontWeight="bold"
+            color={colorMode === 'light' ?  'gray.500' : 'white'}
+          >
+            +
+          </Text>
+          <Nextdotjs color={colorMode === 'light' ? '#718096' : '#E2E8F0'} height={iconSize} width={iconSize}/> 
+          <Text 
+            as="h4"
+            fontSize={['lg', 'xl', 'lg', '2xl']}
+            fontWeight="light"
+            color={colorMode === 'light' ?  'gray.700' : 'white'}
+          >
+            with 💖
+          </Text>
+        </Stack>
       </Stack>
-      <Stack w={['100%', '100%', '50%']}>
+      <Stack
+        direction="row"
+        justify="center"
+        w={['100%', '100%', '50%']}
+      >
         <ProfileDesktop />
         <ProfileMobile />
       </Stack>
