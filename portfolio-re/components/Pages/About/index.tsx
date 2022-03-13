414a6697346c0d04a25d58c26b16b8e93bc20c02
Sean Christian Lozana <lozanasc@gmail.com>
2022-03-13 18:12:32 +0800

    Fix:
    Font size for Homepage, flex direction, and icon sizes for About age

--- portfolio-re/components/Pages/About/index.tsx
+++ portfolio-re/components/Pages/About/index.tsx
@@ -1,20 +1,30 @@
-import { Stack, Heading, Text, Divider, useBreakpointValue } from "@chakra-ui/react";
+import { Stack, Heading, Text, Divider } from "@chakra-ui/react";
 import { Chakraui, Nextdotjs } from '@icons-pack/react-simple-icons';
+import { useEffect, useState } from "react";
 import { ProfileDesktop, ProfileMobile } from './components';
 
 type AboutProps = {
   colorMode: string
 }
 
+// TODO: Fix dark-mode font size and tech icon size
+// * 
 const About = ({colorMode}:AboutProps) => {
 
-  const iconSize = useBreakpointValue([24, 32, 32])
+  const [ iconSize, setIconSize ] = useState(0)
+  
+  useEffect(() => {
+    if(window.innerWidth > 767)
+      setIconSize(32)
+    else if(window.innerWidth < 767)
+      setIconSize(24)
+  }, [])
 
   return (
     <Stack
       w="100%"
       m="auto"
-      direction={['column-reverse', 'column', 'row']}
+      direction={['column-reverse', 'column-reverse', 'row']}
       justify="space-evenly">
       <Stack
         m="auto"
@@ -66,7 +76,7 @@ const About = ({colorMode}:AboutProps) => {
           <Chakraui color={colorMode === 'light' ? '#718096' : '#E2E8F0'} height={iconSize} width={iconSize}/>
           <Text 
             as="h4"
-            fontSize={['lg', 'xl', 'lg', '2xl']}
+            fontSize={['lg', 'lg', 'lg', '2xl']}
             fontWeight="bold"
             color={colorMode === 'light' ?  'gray.500' : 'white'}
           >
@@ -75,7 +85,7 @@ const About = ({colorMode}:AboutProps) => {
           <Nextdotjs color={colorMode === 'light' ? '#718096' : '#E2E8F0'} height={iconSize} width={iconSize}/> 
           <Text 
             as="h4"
-            fontSize={['lg', 'xl', 'lg', '2xl']}
+            fontSize={['lg', 'lg', 'lg', '2xl']}
             fontWeight="light"
             color={colorMode === 'light' ?  'gray.700' : 'white'}
           >
