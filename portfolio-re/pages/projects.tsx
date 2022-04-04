0a2c091ce4c12f6bd738516bb78f6136491fda74
Sean Christian Lozana <lozanasc@gmail.com>
2022-04-05 06:39:19 +0800

    Used SWR to client-side fetch my repo instead of SSR

--- portfolio-re/pages/projects.tsx
+++ portfolio-re/pages/projects.tsx
@@ -1,10 +1,9 @@
-import { Octokit } from '@octokit/core'
 import type { NextPage, GetServerSideProps } from 'next'
 import { useColorMode } from '@chakra-ui/react'
 import Layout from '../components/Layout'
 import ProjectPage from '../components/Pages/Projects'
 
-const Project: NextPage = ({ RepositoryList }: any) => {
+const Project: NextPage = () => {
   const { colorMode, toggleColorMode } = useColorMode()
   return (
     <Layout 
@@ -12,29 +11,9 @@ const Project: NextPage = ({ RepositoryList }: any) => {
       colorMode={colorMode} 
       toggleColorMode={toggleColorMode}
     >
-      <ProjectPage colorMode={colorMode} repoList={RepositoryList} />
+      <ProjectPage colorMode={colorMode} />
     </Layout>
   )
-}
-
-export const getServerSideProps: GetServerSideProps = async(context) => {
-
-  const octokit = new Octokit({ auth: process.env.GITHUB_API_KEY });
-
-  // TODO: Find a way to grab the Social Preview for each repository through their REST API
-  const GetRepositoryList = await octokit.request('GET /user/repos', {
-    type: 'public',
-    sort: 'updated'
-  })
-
-  const { data: RepositoryList } = GetRepositoryList
-
-  // TODO: Too big, need to filter shitty projects from the best one...
-  return {
-    props: {
-      RepositoryList
-    }
-  }
 }
 
 export default Project
