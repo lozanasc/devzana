a230e8a0c57b193e12f26e2d6d5da0208f5f81ed
Sean Christian Lozana <lozanasc@gmail.com>
2022-04-16 22:32:33 +0800

    Added custom theming, and set default colorMode to dark

--- portfolio-re/pages/_app.tsx
+++ portfolio-re/pages/_app.tsx
@@ -1,13 +1,12 @@
 import type { AppProps } from 'next/app'
-import { ChakraProvider, extendTheme } from '@chakra-ui/react'
-import { theme, components } from '../styles/custom'
+import { ChakraProvider } from '@chakra-ui/react'
+import { theme } from '../styles/custom'
 import '../styles/custom.css'
 
-const custom = extendTheme([theme, components])
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={custom}>
+    <ChakraProvider theme={theme}>
         <Component {...pageProps} />
     </ChakraProvider>
   )
